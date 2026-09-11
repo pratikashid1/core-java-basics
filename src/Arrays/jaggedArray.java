3bf0a5e55cf3da3bad5144f73c4b4524e27ce847
Pratik Kashid <kashidpratik100@gmail.com>
2026-09-11 23:09:10 +0530

    Day 13 - Jagged Array

---
+++ src/Arrays/jaggedArray.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class jaggedArray {
+    public static void main(String[] args) {
+        // Declaring 2D array with 2 rows
+        int arr[][] = new int[2][];
+
+        // Making the above array Jagged
+        arr[0] = new int[3];
+        arr[1] = new int[2];
+
+        // Initializing array
+        int count = 0;
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = count++;
+            }
+        }
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
